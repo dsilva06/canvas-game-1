38b1e135592ce730f8339ef46afb4de161613c21
Diego Silva <Diegosilva06092001@gmail.com>
2022-01-24 13:15:23 -0400

    creating enemies

--- js/index.js
+++ js/index.js
@@ -27,23 +27,45 @@ class Projectile extends Player {
     this.color = color;
     this.velocity = velocity;
   }
-  draw() {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    ctx.fillStyle = this.color;
-    ctx.fill();
-  }
   update() {
     this.draw();
     this.x = this.x + this.velocity.x;
     this.y = this.y + this.velocity.y;
   }
 }
+class Enemy extends Projectile {}
 
 const shooter = new Player(this.x, this.y, 30, "black");
 
-
 const projectiles = [];
+const enemies = [];
+
+function spawnEnemies() {
+  setInterval(() => {
+    const radius = Math.random() * (30 - 5) + 5
+    let x
+    let y
+
+    if(Math.random() < 0.5) {
+
+       x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+       y = Math.random() * canvas.height
+    }else{
+      x = Math.random() * canvas.width
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+    }
+    const color = "white";
+
+    const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+    
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle),
+    };
+
+    enemies.push(new Enemy(x, y, radius, color, velocity));
+  }, 1000);
+}
 
 function animate() {
   requestAnimationFrame(animate);
@@ -51,6 +73,9 @@ function animate() {
   shooter.draw();
   projectiles.forEach((projectile) => {
     projectile.update();
+  });
+  enemies.forEach((enemy) => {
+    enemy.update();
   });
 }
 
@@ -60,10 +85,11 @@ addEventListener("click", (event) => {
     event.clientX - canvas.width / 2
   );
   const velocity = {
-    x : Math.cos(angle),
-    y : Math.sin(angle)
-  }
+    x: Math.cos(angle),
+    y: Math.sin(angle),
+  };
 
   projectiles.push(new Projectile(this.x, this.y, 10, "blue", velocity));
 });
 animate();
+spawnEnemies();
